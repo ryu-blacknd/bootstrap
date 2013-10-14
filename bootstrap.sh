2ed07f95c74c4ae51773210df4fafed3a0e3b045
Ryuichi Sano <ryu@blacknd.com>
2013-10-14 22:38:40 +0900

    add messages

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,23 +1,30 @@
 #!/bin/sh
 
 #
-# Security
+# Stop SeLinux
 #
+echo -e "\033[0;32m[Stop selinux]\033[0;39m"
 setenforce 0
 sed -i -e 's/^SELINUX=enforcing/SELINUX=disabled/' /etc/sysconfig/selinux
 
+#
+# Stop iptables
+#
+echo -e "\033[0;32m[Stop iptables]\033[0;39m"
 /sbin/iptables -F
 /sbin/service iptables stop
 
 #
-# SSH
+# SSH Setting
 #
-sed -i -e 's/^#PermitRootLogin yes/PermitRootLogin no/' /etc/ssh/sshd_config
+echo -e "\033[0;32m[SSH Setting]\033[0;39m"
+# sed -i -e 's/^#PermitRootLogin yes/PermitRootLogin no/' /etc/ssh/sshd_config
 sed -i -e 's/^#PermitEmptyPasswords/PermitEmptyPasswords/' /etc/ssh/sshd_config
 
 #
 # Stop Service
 #
+echo -e "\033[0;32m[Stop Service]\033[0;39m"
 if [ -f '/etc/rc.d/init.d/iptables'  ]; then chkconfig iptables off;  fi
 if [ -f '/etc/rc.d/init.d/ip6tables' ]; then chkconfig ip6tables off; fi
 if [ -f '/etc/rc.d/init.d/iscsi'     ]; then chkconfig iscsi off;     fi
@@ -28,17 +35,22 @@ if [ -f '/etc/rc.d/init.d/udev-post' ]; then chkconfig udev-post off; fi
 #
 # yum repository
 #
+echo -e "\033[0;32m[yum repository]\033[0;39m"
 rpm -Uvh http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
 rpm -Uvh http://rpms.famillecollet.com/enterprise/remi-release-6.rpm
-
 rpm --import http://download.fedoraproject.org/pub/epel/RPM-GPG-KEY-EPEL
 rpm --import http://rpms.famillecollet.com/RPM-GPG-KEY-remi
 
 #
 # yum Install
 #
+echo -e "\033[0;32m[yum update]\033[0;39m"
 yum --enablerepo=remi -y update
+
+echo -e "\033[0;32m[yum install Development Tools]\033[0;39m"
 yum --enablerepo=remi -y groupinstall "Development Tools"
+
+echo -e "\033[0;32m[yum install others]\033[0;39m"
 yum --enablerepo=remi -y install man lv w3m vim wget yum-cron openssl-devel readline-devel zlib-devel curl-devel libyaml-devel ImageMagick ImageMagick-devel ipa-pgothic-fonts ntp httpd httpd-devel mysql-server mysql-devel php php-cli php-pdo php-mbstring php-mcrypt php-mysql php-devel php-common php-pear php-gd php-xml php-pecl-xdebug php-pecl-imagick
 
 chkconfig yum-cron on
@@ -46,6 +58,7 @@ chkconfig yum-cron on
 #
 # Shell & Editor
 #
+echo -e "\033[0;32m[Shell & Editor]\033[0;39m"
 cd
 mkdir -p ~/.vim/bundle
 git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
@@ -58,6 +71,7 @@ chmod 755 ~/molokai.sh
 #
 # Git
 #
+echo -e "\033[0;32m[Git]\033[0;39m"
 git config --global http.sslverify false
 useradd devuser -p devuser
 mkdir /var/repos
@@ -66,12 +80,14 @@ chown devuser. /var/repos
 #
 # Sudoers
 #
+echo -e "\033[0;32m[Sudoers]\033[0;39m"
 sed -i -e 's/^Defaults    requiretty/#Defaults    requiretty/' /etc/sudoers
 echo "\n\ndevuser ALL = NOPASSWD: /usr/bin/git/" >> /etc/sudoers
 
 #
 # ntp
 #
+echo -e "\033[0;32m[Start ntp]\033[0;39m"
 /sbin/chkconfig ntpd on
 /sbin/service ntpd start
 
@@ -83,11 +99,13 @@ echo "\n\ndevuser ALL = NOPASSWD: /usr/bin/git/" >> /etc/sudoers
 #
 # PHP
 #
+echo -e "\033[0;32m[PHP Setting]\033[0;39m"
 sed -i -e "s/^\[Date\]$/[Date]\ndate.timezone = 'Asia\/Tokyo'/" /etc/php.ini
 
 #
 # MySQL
 #
+echo -e "\033[0;32m[MySQL Setting]\033[0;39m"
 sed -i -e "s/^\[mysqld\]$/[mysqld]\ncharacter-set-server = utf8/" /etc/my.cnf
 sed -i -e "s/^\[mysql\]$/[mysql]\ndefault-character-set = utf8/" /etc/my.cnf
 sed -i -e "s/^\[mysqldump\]$/[mysqldump]\ndefault-character-set = utf8/" /etc/my.cnf
@@ -106,6 +124,7 @@ mysql -u root -e "$SQL"
 #
 # phpMyAdmin
 #
+echo -e "\033[0;32m[phpMyAdmin Install]\033[0;39m"
 cd
 wget http://downloads.sourceforge.net/project/phpmyadmin/phpMyAdmin/4.0.8/phpMyAdmin-4.0.8-all-languages.zip
 unzip phpMyAdmin-4.0.8-all-languages.zip -d /var/www/html
@@ -115,6 +134,7 @@ chown -R apache. /var/www/html
 #
 # Ruby & bundler
 #
+echo -e "\033[0;32m[Ruby Install]\033[0;39m"
 yum -y remove ruby
 yum -y remove ruby-*
 mkdir src
@@ -131,6 +151,7 @@ cd ..
 #
 # Redmine
 #
+echo -e "\033[0;32m[Redmine Install]\033[0;39m"
 svn co http://svn.redmine.org/redmine/branches/2.3-stable /var/lib/redmine
 CNF="production:
   adapter: mysql2
@@ -160,6 +181,7 @@ RAILS_ENV=production bundle exec rake db:migrate
 #
 # Passenger
 #
+echo -e "\033[0;32m[Passenger Install]\033[0;39m"
 gem install passenger --no-rdoc --no-ri
 passenger-install-apache2-module
 CNF="LoadModule passenger_module /usr/local/lib/ruby/gems/1.9.1/gems/passenger-4.0.20/buildout/apache2/mod_passenger.so
@@ -189,6 +211,7 @@ ln -s /var/lib/redmine/public /var/www/html/redmine
 #
 # Redmine themes
 #
+echo -e "\033[0;32m[Redmine themes]\033[0;39m"
 cd /var/lib/redmine/public/themes
 git clone https://github.com/farend/redmine_theme_farend_fancy.git
 git clone https://github.com/makotokw/redmine-theme-gitmike.git
@@ -196,6 +219,7 @@ git clone https://github.com/makotokw/redmine-theme-gitmike.git
 #
 # Redmine plugins
 #
+echo -e "\033[0;32m[Redmine plugins]\033[0;39m"
 cd /var/lib/redmine/plugins
 
 wget https://bitbucket.org/haru_iida/redmine_wiki_extensions/downloads/redmine_wiki_extensions-0.6.4.zip
@@ -217,5 +241,7 @@ bundle exec rake redmine:plugins:migrate RAILS_ENV=production
 #
 # Start Service
 #
+echo -e "\033[0;32m[Start Service]\033[0;39m"
 /sbin/service httpd start
 /sbin/service mysqld restart
+
---
+++ readme.md
@@ -0,0 +1,4 @@
+# CentOS64 bootstrap script
+
+- [CentOS](http://www.centos.org/) : newest x86/64 minimal iso image
+-
