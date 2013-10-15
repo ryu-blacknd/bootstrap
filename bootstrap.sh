7ff5908923c9391f45da4b3499c8ff3ddee735ed
Ryuichi Sano <ryu@blacknd.com>
2013-10-15 09:57:58 +0900

    fix sudoers line

--- bootstrap.sh
+++ bootstrap.sh
@@ -85,7 +85,7 @@ chown devuser. /var/www/html
 #
 echo -e "\033[0;32m[Sudoers]\033[0;39m"
 sed -i -e 's/^Defaults    requiretty/#Defaults    requiretty/' /etc/sudoers
-echo "devuser ALL = NOPASSWD: /usr/bin/git/" >> /etc/sudoers
+echo "devuser ALL = NOPASSWD: /usr/bin/git" >> /etc/sudoers
 
 #
 # ntp
