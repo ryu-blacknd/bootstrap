792b9fe7386121cd206f01e8752ed788b65d599b
root <root@devserver.netartz.local>
2013-10-28 17:41:20 +0900

    fix makerepo

--- makerepo.sh
+++ makerepo.sh
@@ -10,7 +10,7 @@ git clone /var/repos/${PROJ_NAME} /var/www/html/${PROJ_NAME}
 
 cp $HOME/post-update /var/repos/${PROJ_NAME}/hooks/
 
-sed -i -e "s/^PROJ_NAME=\[PROJ_NAME\]/PROJ_NAME=${PROJ_NAME}/" /var/repos/${PROJ_NAME}/hooks/post-update
+sed -i -e "s/^PROJ_NAME=/PROJ_NAME=${PROJ_NAME}/" /var/repos/${PROJ_NAME}/hooks/post-update
 
 done
 
