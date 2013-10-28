ea1d64c7b274089ef9f5d20511b461ccfd3233ca
ryu_blacknd <ryu@blacknd.com>
2013-10-28 14:32:47 +0900

    fix post-update

---
+++ makerepo.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+for PROJ_NAME in $@
+
+do
+
+git init --bare /var/repos/${PROJ_NAME}
+
+git clone /var/repos/${PROJ_NAME} /var/www/html/${PROJ_NAME}
+
+cp $HOME/post-update /var/repos/${PROJ_NAME}/hooks/
+
+sed -i -e "s/^PROJ_NAME=\[PROJ_NAME\]/PROJ_NAME=${PROJ_NAME}/" /var/repos/${PROJ_NAME}/hooks/post-update
+
+done
+
